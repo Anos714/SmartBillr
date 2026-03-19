d22de9c70f105ac39d925f3ae628ee980697e3ec
Anos714 <sainrahul374@gmail.com>
2026-03-19 17:40:41 +0530

    feat:intialized the project and added clerk publishabel key

---
+++ frontend/src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/react";
+
+const PublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PublishableKey) {
+  throw new Error(
+    "VITE_CLERK_PUBLISHABLE_KEY is not defined in the environment variables.",
+  );
+}
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PublishableKey}>
+      <App />
+    </ClerkProvider>
+  </StrictMode>,
+);
