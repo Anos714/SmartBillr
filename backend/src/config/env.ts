afae9ebf6ead2d7685a945f05b5f87a642eeeb14
Anos714 <sainrahul374@gmail.com>
2026-03-21 21:43:24 +0530

    feat:added clerk keys and api base url endpoint in env.ts file

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -3,4 +3,7 @@ import "dotenv/config";
 export const env = {
   PORT: process.env.PORT || 8000,
   MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/smartbillr",
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  API_BASE_URL: process.env.API_BASE_URL,
 };
