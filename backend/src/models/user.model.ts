76bb5e9c85b640b85268ddf5e52cbe68d478c7e3
Anos714 <sainrahul374@gmail.com>
2026-04-22 14:48:47 +0530

    feat: implement user model with schema and interface for MongoDB

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,1 +1,50 @@
-// import 
+import { Document, Schema, model } from "mongoose";
+
+interface IUser extends Document {
+  fullName: string;
+  email: string;
+  password: string;
+  role: "user" | "admin";
+  plan: "free" | "pro";
+  isUserVerified: boolean;
+  tokenVersion: number;
+  resetPassToken: string | undefined;
+  resetPassTokenExpiry: string | undefined;
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    role: { type: String, enum: ["user", "admin"], deafult: "user" },
+    plan: { type: String, enum: ["free", "pro"], default: "free" },
+    isUserVerified: {
+      type: Boolean,
+      default: false,
+    },
+    tokenVersion: {
+      type: Number,
+      deafult: 0,
+    },
+    resetPassToken: { type: String, default: undefined },
+    resetPassTokenExpiry: { type: Date, default: undefined },
+  },
+  { timestamps: true },
+);
+
+export const User = model("User", UserSchema);
